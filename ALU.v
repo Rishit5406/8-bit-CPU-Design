d8137d6c2d6c689724fb727a15f6ba0c50337207
Rishit Goyal <rishitgoyal1803@gmail.com>
2026-09-06 17:38:05 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-
 module ALU(
     input wire [7:0]a,
     input wire [7:0]b,
@@ -7,19 +6,15 @@ module ALU(
     output reg [7:0]y,
     output wire z
     );
-    
     always@(*) begin
         case(sel)
-            3'b000 : y = b;      //loads the instruction(ACC <- IMM)
-            3'b001 : y = a + b;  //ADD ; hardware it will be an 8 bit adder to 1 input of MUX
-            3'b010 : y = a - b;  //SUB ; implemented using adder and 2s complement internally 
-            3'b011 : y = a & b;  //AND ;8 parallel AND gates
-            3'b100 : y = a | b;  //OR  ;8 parallel or gates
-            default : y = 8'h00; //if sel is none of values listed above set outpyt y to 0
-        endcase                  //without default synthesizer creates latch(unwanted memory)
+            3'b000 : y = b;      
+            3'b001 : y = a + b;  
+            3'b010 : y = a - b;  
+            3'b011 : y = a & b;  
+            3'b100 : y = a | b;  
+            default : y = 8'h00; 
+        endcase                  
     end
-    
     assign z = (y == 8'h00);
-      
 endmodule
-
